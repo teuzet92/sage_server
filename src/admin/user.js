b78306a4826988d7b49f444d5d0f56e6821ae314
teuzet <zhimkov@game-insight.com>
2023-12-18 17:04:59 +0100

    Создание юзеров в админке

--- src/admin/user.js
+++ src/admin/user.js
@@ -1,10 +1,10 @@
 module.exports = class extends getClass('storage/model/model') {
 
 	async cmd_login({ password }) {
+		assert(password == this.values.password, 'Wrong password');
 		let session = uuid();
 		this.values.session = session;
 		await this.save();
-		return session;
+		return this.saveData();
 	}
-
 }
---
+++ src/admin/users.js
@@ -0,0 +1,17 @@
+module.exports = class extends getClass('storage/storage') {
+
+	async cmd_createUser({ id }) {
+		let users = await this.getAll({ id });
+		assert(users.length == 0, `User with id '${id}' already exists`);
+		let user = this.createModel({ id });
+		await user.save();
+		return user.saveData();
+	}
+
+	async cmd_validateSession({ session }) {
+		let users = await this.getAll({ 'values.session': session });
+		assert(users.length == 1, 'Unkown or expired session');
+		return users[0].saveData();
+	}
+
+}
